9bae9ca2cc9edac66795d637b697d7d3b66f016a
Ulf Zackrisson <ulmezz@gmail.com>
2019-04-09 13:15:22 +0200

    Register page init

--- react-client/src/components/App.js
+++ react-client/src/components/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 import StartPage from './main/pages/startPage/StartPage';
 import HeaderPage from './main/pages/header/HeaderPage';
+import RegisterPage from './main/pages/registerPage/RegisterPage';
 
 import './App.css';
 
@@ -38,6 +39,7 @@ class App extends Component {
       <div className="container">
         <HeaderPage />
         <Route exact path="/" component={StartPage} />
+        <Route exact path="/register" component={RegisterPage} />
       </div>
     );
   }
