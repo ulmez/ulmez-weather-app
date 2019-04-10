84554132d906701acc61ccc262e24e7c9c0e4646
Ulf Zackrisson <ulmezz@gmail.com>
2019-04-10 12:33:07 +0200

    Removed comments

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,8 +5,6 @@ var bcrypt = require('bcrypt');
 var User = require('../models/User');
 
 module.exports.register_user = function(req, res, next) {
-    // console.log('Gick in?');
-    // res.status(200).json({message: 'Success!!!!'});
     User.find({
         username: req.body.email
     })
@@ -35,37 +33,18 @@ module.exports.register_user = function(req, res, next) {
                     };
 
                     const userObj = new User(itemUser);
+
                     userObj.save((err) => {
-                        if (err) return res.status(500).json({message: err});
-                        return res.status(200).json({message: userObj});
+                        if (err) {
+                            return res.status(500).json({
+                                message: err
+                            });
+                        }
+
+                        return res.status(200).json({
+                            message: userObj
+                        });
                     });
-                
-                    // var itemPerson = {
-                    //     first_name: req.body.first_name,
-                    //     last_name: req.body.last_name,
-                    //     social_security_number: req.body.social_security_number,
-                    //     address: req.body.address,
-                    //     postal_code: req.body.postal_code,
-                    //     phone_home: req.body.phone_home,
-                    //     phone_mobile: req.body.phone_mobile,
-                    //     email: req.body.email,
-                    //     user_id: {$ojFuture: "0._id"}
-                    // };
-                
-                    // task
-                    // .save(User, itemUser)
-                    // .save(Person, itemPerson)
-                    // .run({useMongoose: true})
-                    // .then(function(results){
-                    //     res.json({
-                    //         message: results
-                    //     });
-                    // })
-                    // .catch(function(err) {
-                    //     res.json({
-                    //         message: err
-                    //     });
-                    // });
                 }
             });
         }
