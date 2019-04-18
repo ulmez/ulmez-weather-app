aaddaf2565f483a2a70b82e4e058f3b3a56da7d9
Ulf Zackrisson <ulmezz@gmail.com>
2019-04-18 15:00:18 +0200

    Fix color for listbox depending on temperature

--- react-client/src/components/main/pages/listBox/ListBox.js
+++ react-client/src/components/main/pages/listBox/ListBox.js
@@ -11,7 +11,8 @@ class ListBox extends React.Component {
         super(props);
 
         this.state = {
-            setWeatherIcon: ''
+            setWeatherIcon: '',
+            RGBColor: ''
         };
     }
 
@@ -40,6 +41,88 @@ class ListBox extends React.Component {
                 }
             }
         }
+
+        this.getRGBTemperature();
+    }
+
+    colorOfTemperature(arrRGBColor1, arrRGBColor2, numberOfGrades, temperature) {
+        let checkAllowedTemperature;
+    
+        // Block to make span of colors restricted to numberOfGrades parameter.
+        // If a city has temperature 33 and numberOfGrades are declared to 30,
+        // temperature will be changed to 30 so the color range will be intact.
+        if (temperature > numberOfGrades) {
+            checkAllowedTemperature = numberOfGrades;
+        } else if (temperature < -numberOfGrades) {
+            checkAllowedTemperature = -numberOfGrades;
+        } else {
+            checkAllowedTemperature = temperature;
+        }
+    
+        let r1, g1, b1, r2, g2, b2;
+    
+        // RGB color for start of range
+        r1 = arrRGBColor1[0];
+        g1 = arrRGBColor1[1];
+        b1 = arrRGBColor1[2];
+    
+        // RGB color for end of range
+        r2 = arrRGBColor2[0];
+        g2 = arrRGBColor2[1];
+        b2 = arrRGBColor2[2];
+    
+        let stepR, stepG, stepB;
+    
+        // Block to get the difference between starting
+        // RGB color and ending RGB color even if the
+        // starting color has a lower or higher value.
+        if (r1 < r2) {
+            stepR = (r2 - r1) / numberOfGrades;
+        } else if (r1 > r2) {
+            stepR = -(r1 - r2) / numberOfGrades;
+        } else {
+            stepR = 0;
+        }
+    
+        if (g1 < g2) {
+            stepG = (g2 - g1) / numberOfGrades;
+        } else if (g1 > g2) {
+            stepG = -(g1 - g2) / numberOfGrades;
+        } else {
+            stepG = 0;
+        }
+    
+        if (b1 < b2) {
+            stepB = (b2 - b1) / numberOfGrades;
+        } else if (b1 > b2) {
+            stepB = -(b1 - b2) / numberOfGrades;
+        } else {
+            stepB = 0;
+        }
+    
+        // Change a negative value to a positive value
+        if (checkAllowedTemperature < 0) {
+            checkAllowedTemperature = -checkAllowedTemperature;
+        }
+    
+        // Sets the RGB color to be used for the specific temperature
+        let tempR = r1 + Math.floor(stepR * checkAllowedTemperature);
+        let tempG = g1 + Math.floor(stepG * checkAllowedTemperature);
+        let tempB = b1 + Math.floor(stepB * checkAllowedTemperature);
+    
+        return [tempR, tempG, tempB];
+    }
+
+    getRGBTemperature() {
+        if (this.props.current.temp_c >= 0) {
+            this.setState({
+                RGBColor: this.colorOfTemperature([0, 158, 229], [191, 64, 0], 30, this.props.current.temp_c)
+            });
+		} else {
+            this.setState({
+                RGBColor: this.colorOfTemperature([0, 158, 229], [40, 0, 102], 30, this.props.current.temp_c)
+            });
+		}
     }
 
     render() {
@@ -47,16 +130,16 @@ class ListBox extends React.Component {
             <div className="list-box background-outer col-sm-12 col-md-6 col-lg-4 col-xl-4">
                 <div className="row p-2 align-self-center">
                     <div className="col-sm-2 d-md-none d-lg-none d-xl-none"></div>
-                    <div className="d-flex align-items-center list-box background-inner box-height col-3 col-sm-2 col-md-3 col-lg-4 col-xl-3">
+                    <div className="d-flex align-items-center list-box box-height col-3 col-sm-2 col-md-3 col-lg-4 col-xl-3" style={{background: 'rgb(' + this.state.RGBColor[0] + ', ' + this.state.RGBColor[1] + ', ' + this.state.RGBColor[2] + ')'}}>
                         <span className="icon" data-icon={this.state.setWeatherIcon}></span>
                     </div>
-                    <div className="d-flex align-items-center list-box background-inner box-height col-7 col-sm-5 col-md-7 col-lg-6 col-xl-7">
+                    <div className="d-flex align-items-center list-box box-height col-7 col-sm-5 col-md-7 col-lg-6 col-xl-7" style={{background: 'rgb(' + this.state.RGBColor[0] + ', ' + this.state.RGBColor[1] + ', ' + this.state.RGBColor[2] + ')'}}>
                         <div className="row">
                         <div className="list-box celsius-design col-12 text-center">{this.props.current.temp_c} <img src={celcius_icon} className="align-top" /></div>
                         <div className="col-12 text-center">{this.props.location.name}, {this.props.location.country}</div>
                         </div>
                     </div>
-                    <div className="d-flex justify-content-end list-box background-inner box-height col-2 col-sm-1 col-md-2 col-lg-2 col-xl-2 p-0">
+                    <div className="d-flex justify-content-end list-box box-height col-2 col-sm-1 col-md-2 col-lg-2 col-xl-2 p-0" style={{background: 'rgb(' + this.state.RGBColor[0] + ', ' + this.state.RGBColor[1] + ', ' + this.state.RGBColor[2] + ')'}}>
                         <span onClick={this.props.deleteBox} className="list-box celsius-design cursor-pointer fa fa-times-circle mt-1 mr-2"></span>
                     </div>
                     <div className="col-sm-2 d-md-none d-lg-none d-xl-none"></div>
