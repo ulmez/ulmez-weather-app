d91c42c9407ca6a79498847115e49eef3a09dfa8
Ulf Zackrisson <ulmezz@gmail.com>
2019-04-18 13:41:54 +0200

    Fix to get right weather icon to listbox component

--- react-client/src/components/main/pages/listBox/ListBox.js
+++ react-client/src/components/main/pages/listBox/ListBox.js
@@ -1,17 +1,54 @@
 import React from 'react';
 
+import weathers from '../../../../inits/init';
+
 import celcius_icon from '../../../../images/c.svg';
 import '../../../../fonts/meteocons-webfont/stylesheet.css';
 import './ListBox.css';
 
 class ListBox extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            setWeatherIcon: ''
+        };
+    }
+
+    componentDidMount() {
+        console.log(this.props.location);
+        console.log(this.props.current);
+        console.log(this.props.condition);
+        console.log(weathers.types);
+
+        if(this.props.current.is_day === 1) {
+            for(let i = 0; i < weathers.types.length; i++) {
+                if(weathers.types[i].code === this.props.condition.code) {
+                    this.setState({
+                        setWeatherIcon: weathers.types[i].night_or_day_icon[1]
+                    });
+                    break;
+                }
+            }
+        } else {
+            for(let i = 0; i < weathers.types.length; i++) {
+                if(weathers.types[i].code === this.props.condition.code) {
+                    this.setState({
+                        setWeatherIcon: weathers.types[i].night_or_day_icon[0]
+                    });
+                    break;
+                }
+            }
+        }
+    }
+
     render() {
         return (
             <div className="list-box background-outer col-sm-12 col-md-6 col-lg-4 col-xl-4">
                 <div className="row p-2 align-self-center">
                     <div className="col-sm-2 d-md-none d-lg-none d-xl-none"></div>
                     <div className="d-flex align-items-center list-box background-inner box-height col-3 col-sm-2 col-md-3 col-lg-4 col-xl-3">
-                        <span className="icon" data-icon="B"></span>
+                        <span className="icon" data-icon={this.state.setWeatherIcon}></span>
                     </div>
                     <div className="d-flex align-items-center list-box background-inner box-height col-7 col-sm-5 col-md-7 col-lg-6 col-xl-7">
                         <div className="row">
---
+++ react-client/src/inits/init.js
@@ -0,0 +1,128 @@
+const weathers = {
+	"types": [{
+		"code": 1000,
+		"night_or_day_icon": ["C", "B"]
+	}, {
+		"code": 1003,
+		"night_or_day_icon": ["I", "H"]
+	}, {
+		"code": 1006,
+		"night_or_day_icon": ["N", "N"]
+	}, {
+		"code": 1009,
+		"night_or_day_icon": ["L", "L"]
+	}, {
+		"code": 1030,
+		"night_or_day_icon": ["M", "M"]
+	}, {
+		"code": 1063,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1066,
+		"night_or_day_icon": ["U", "U"]
+	}, {
+		"code": 1072,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1087,
+		"night_or_day_icon": ["O", "O"]
+	}, {
+		"code": 1114,
+		"night_or_day_icon": ["T", "T"]
+	}, {
+		"code": 1117,
+		"night_or_day_icon": ["T", "T"]
+	}, {
+		"code": 1135,
+		"night_or_day_icon": ["M", "M"]
+	}, {
+		"code": 1147,
+		"night_or_day_icon": ["M", "M"]
+	}, {
+		"code": 1150,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1153,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1168,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1171,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1180,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1183,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1186,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1189,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1192,
+		"night_or_day_icon": ["R", "R"]
+	}, {
+		"code": 1195,
+		"night_or_day_icon": ["R", "R"]
+	}, {
+		"code": 1198,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1201,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1210,
+		"night_or_day_icon": ["U", "U"]
+	}, {
+		"code": 1213,
+		"night_or_day_icon": ["U", "U"]
+	}, {
+		"code": 1216,
+		"night_or_day_icon": ["U", "U"]
+	}, {
+		"code": 1219,
+		"night_or_day_icon": ["U", "U"]
+	}, {
+		"code": 1222,
+		"night_or_day_icon": ["X", "X"]
+	}, {
+		"code": 1225,
+		"night_or_day_icon": ["X", "X"]
+	}, {
+		"code": 1237,
+		"night_or_day_icon": ["Y", "Y"]
+	}, {
+		"code": 1240,
+		"night_or_day_icon": ["Q", "Q"]
+	}, {
+		"code": 1243,
+		"night_or_day_icon": ["R", "R"]
+	}, {
+		"code": 1246,
+		"night_or_day_icon": ["R", "R"]
+	}, {
+		"code": 1255,
+		"night_or_day_icon": ["U", "U"]
+	}, {
+		"code": 1258,
+		"night_or_day_icon": ["X", "X"]
+	}, {
+		"code": 1261,
+		"night_or_day_icon": ["Y", "Y"]
+	}, {
+		"code": 1264,
+		"night_or_day_icon": ["Y", "Y"]
+	}, {
+		"code": 1273,
+		"night_or_day_icon": ["O", "O"]
+	}, {
+		"code": 1276,
+		"night_or_day_icon": ["O", "O"]
+	}]
+};
+
+export default weathers;
